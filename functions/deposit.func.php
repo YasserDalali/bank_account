14aa9de2bf8482e7e6557321ad41db21b17a756f
YasserDalali <yasser.dalali.personal@gmail.com>
2024-05-13 16:07:37 +0100

    simplified deposit.func, loans are now functional

--- functions/deposit.func.php
+++ functions/deposit.func.php
@@ -3,34 +3,26 @@
             $sql = "SELECT * FROM account WHERE accId='{$accToSend}'";
         $result = $pdo->query($sql);
         
-        if ($result) {
-            $temp = $result->fetch(PDO::FETCH_ASSOC);
+        if ($result) {        
         
-        
-                $sql = "UPDATE account  SET balance = (balance+{$ammount})  WHERE accId='{$accToSend}'";
+                $sql = "UPDATE account SET balance = (balance+{$ammount})  WHERE accId='{$accToSend}'";
                 $result = $pdo->query($sql);
         
-                $sql = "INSERT INTO trans (ammount, fromacc, toacc) VALUES ('{$ammount}', 'Central Bank', 
+                $sql = "INSERT INTO trans (ammount, fromacc, toacc) VALUES ('{$ammount}', 2, 
                 '{$accToSend}')";
                 $result = $pdo->query($sql);
-        
-        
-                echo "Transaction successful";
+
+                echo "DEPOSIT successful";
         
         
         
                 require "refresh.inc.php";
-        
-        
-        
-                $temp = $ammount <= $accountBalance;
-                echo  "<hr>" . $accountBalance . "-{$ammount} = {$temp}" . "<hr>";
-        
-                var_dump($_SESSION);
-        
                 header("Location: ../main.php");
-            } else {
-                echo "Insufficient funds";
+
+            } 
+            
+            else {
+                echo "Unfound user";
             }
         
         }
--- includes/loans.inc.php
+++ includes/loans.inc.php
@@ -2,21 +2,25 @@
 
 require "dbh.inc.php";
 session_start();
+
 $ammount = 5000;
+
 $accountBalance = 0;
 include "../functions/deposit.func.php";
+
 require "refresh.inc.php";
 
-if ($_POST['balance'] >= 0) {
 
-    $sql = "SELECT * FROM loan WHERE loaner='{$_POST['uidUsers']}'";
+if ($_SESSION['balance'] >= 0) {
+
+    $sql = "SELECT * FROM loan WHERE loaner='{$_SESSION['uidUsers']}'";
     $result = $pdo->query($sql);
     
     if ($result) {
 
     
         if (checkCreditScore($result) == true) {
-            deposit($pdo, $ammount, $_POST['uidUsers']);
+            deposit($pdo, $ammount, $_SESSION['uidUsers']);
             echo "Loan granted.";
 
         }
@@ -27,7 +31,7 @@ if ($_POST['balance'] >= 0) {
     }
 
     else {
-        deposit($pdo, $ammount, $_POST['uidUsers']);
+        deposit($pdo, $ammount, $_SESSION['uidUsers']);
         echo "Loan granted.";
 
     }
